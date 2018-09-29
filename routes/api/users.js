e388ae1e03dc81677419cbfccfac1001fe2ebdc8
imrizwan <razzyshaikh@gmail.com>
2018-09-30 02:10:22 +0500

    More Validation in Login and Register

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,6 +10,7 @@ const passport = require('passport');
 // load input validation
 
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.post('/register', (req, res)=> {
 
@@ -56,16 +57,25 @@ router.post('/register', (req, res)=> {
 })
 
 router.post('/login', (req, res)=> {
+
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    // check validation
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
-
+    
 
     //Find user by email
 
     User.findOne({email})
         .then((user)=> {
             if(!user){
-                return res.status(404).json({email: 'User email not found!'});
+                errors.email = 'User email not found!';
+                return res.status(404).json(errors);
             }
 
             // Check password 
@@ -85,7 +95,8 @@ router.post('/login', (req, res)=> {
                                 });
                             });
                     } else {
-                        return res.status(400).json({ password: 'Password Incorrent' })
+                        errors.password = 'Password Incorrent';
+                        return res.status(400).json(errors)
                     }
                 })
         })
